64a2503e58f159c8eba6a277b09adc94393e87d1
aryan1403 <aaryan14032006@email.com>
2022-04-22 14:23:23 +0530

    scanner even odd

--- Basics/evenOdd.java
+++ Basics/evenOdd.java
@@ -1,8 +1,12 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class evenOdd {
     public static void main(String[] args) {
-        int n = 31;
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
         // n % 2 == 0 (even)
         // n % 2 not = 0 (odd)
