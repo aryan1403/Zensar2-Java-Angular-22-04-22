138578b634e8b9884bc4be170ce7d1c823a53db4
aryan1403 <aaryan14032006@email.com>
2022-04-22 14:18:06 +0530

    operators done

---
+++ Basics/operators.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class operators {
+    public static void main(String[] args) {
+        // System.out.println(1 < 2);
+        // System.out.println(2 >= 2);
+        // System.out.println(5 == 5);
+    }
+}
