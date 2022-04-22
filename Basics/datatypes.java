05fabe8db08c50717ee735cb2ede94d61aea7b8d
aryan1403 <aaryan14032006@email.com>
2022-04-22 14:17:58 +0530

    data types done

---
+++ Basics/datatypes.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        // numbers -> Integers
+        int a = -12;
+
+        // point, decimal value
+        double d = 45.6;
+
+        // Word, Sentence
+        String s = "Aaryan is the Trainer";
+
+        // Character
+        char ch = 'a';
+
+        // true, false
+        boolean bool = true;
+    }
+}
