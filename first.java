a8fca77f40092c04902d96655cacf62eb90bf8d0
aryan1403 <aaryan14032006@email.com>
2022-04-22 11:47:14 +0530

    java basics

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first {
+    // Main method
+    public static void main(String[] args) {
+        double a = 50.9;
+        int b = -6;
+
+        System.out.println("Sum =" + (a+b));
+        System.out.println("Subtraction =" + (a-b));
+        System.out.println("Division =" + (a/b));
+        System.out.println("Product =" + (a*b));
+    }
+}
